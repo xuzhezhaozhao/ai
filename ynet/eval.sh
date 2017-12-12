94451bc36f281ca753055c6859dd48163e5b1e18
zhezhaoxu <zhezhaoxu@tencent.com>
2017-12-12 10:31:19 +0800

    update

--- ynet/eval.sh
+++ ynet/eval.sh
@@ -2,4 +2,4 @@
 
 input_prefix=$1
 
-python train.py --learning_rate 0.0001 --epoches 1 --batch_size 50 --keep_prob 0.5 --embedding_dim 100 --num_sampled 100 --watched_size 20 --video_embeddings_file_binary ${input_prefix}.vec --video_embeddings_file_dict ${input_prefix}.dict --train_watched_file ${input_prefix}.watched --train_predicts_file ${input_prefix}.predicts --model_dir model --run_mode eval --k=20 --loss nce --embeddings_trainable true
+python train.py --learning_rate 0.0001 --epoches 1 --batch_size 50 --keep_prob 0.5 --embedding_dim 50 --num_sampled 100 --watched_size 10 --video_embeddings_file_binary ${input_prefix}.vec --video_embeddings_file_dict ${input_prefix}.dict --train_watched_file ${input_prefix}.watched --train_predicts_file ${input_prefix}.predicts --model_dir model --run_mode eval --k=20 --loss nce --embeddings_trainable true
--- ynet/train.sh
+++ ynet/train.sh
@@ -2,4 +2,4 @@
 
 input_prefix=$1
 
-python train.py --learning_rate 0.0001 --epoches 10 --batch_size 500 --keep_prob 0.5 --embedding_dim 50 --num_sampled 100 --watched_size 10 --video_embeddings_file_binary ${input_prefix}.vec --video_embeddings_file_dict ${input_prefix}.dict --train_watched_file ${input_prefix}.watched --train_predicts_file ${input_prefix}.predicts --model_dir model --run_mode train --k=50 --loss nce --embeddings_trainable true
+python train.py --learning_rate 0.01 --epoches 10 --batch_size 500 --keep_prob 0.5 --embedding_dim 50 --num_sampled 100 --watched_size 10 --video_embeddings_file_binary ${input_prefix}.vec --video_embeddings_file_dict ${input_prefix}.dict --train_watched_file ${input_prefix}.watched --train_predicts_file ${input_prefix}.predicts --model_dir model --run_mode train --k=50 --loss nce --embeddings_trainable true
