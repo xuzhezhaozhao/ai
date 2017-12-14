f10b48251c7fa5bcab88ef226b8d2a35bd3843a4
zhezhaoxu <zhezhaoxu@tencent.com>
2017-12-14 19:05:23 +0800

    update

--- ynet/protos/cpp_out/c.sh
+++ ynet/protos/cpp_out/c.sh
@@ -1,6 +1,5 @@
-#g++ -std=c++11 -static -I. client.cc tensorflow/core/example/*.cc tensorflow/core/framework/*.cc tensorflow/core/protobuf/*.cc -L/usr/local/lib -Wl,--no-as-needed -lgrpc++_reflection -Wl,--as-needed -ldl -lprotobuf -lgrpc -lgrpc++ -lpthread -pthread tensorflow/core/lib/core/*.cc tensorflow_serving/apis/*.cc -o client
 
 srcfiles=`find . -name '*.cc'`
 echo ${srcfiles}
 
-g++ -std=c++11 -static -I. -o client ${srcfiles} -lgrpc++_reflection -lgrpc++ -lgrpc  -lprotobuf -pthread
+g++ -std=c++11 -static -I. -o client ${srcfiles} -lgrpc++ -lgrpc -lprotobuf -pthread -ldl -lz
--- ynet/protos/cpp_out/client.cc
+++ ynet/protos/cpp_out/client.cc
@@ -5,8 +5,8 @@
 
 #include <grpc++/grpc++.h>
 
-#include "tensorflow_serving/apis/prediction_service.grpc.pb.h"
 #include "tensorflow_serving/apis/predict.pb.h"
+#include "tensorflow_serving/apis/prediction_service.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -19,54 +19,54 @@ using tensorflow::TensorShapeProto;
 using tensorflow::DataType;
 
 class PredictionClient {
-  public:
-    PredictionClient(std::shared_ptr<Channel> channel)
+ public:
+  PredictionClient(std::shared_ptr<Channel> channel)
       : stub_(PredictionService::NewStub(channel)) {}
 
-    void Predict() {
-      PredictRequest request;
-      auto* model_spec = request.mutable_model_spec();
-      model_spec->set_name("default");
-      model_spec->set_signature_name("predicts");
-      auto* inputs = request.mutable_inputs();
-      TensorProto input_tensor;
-      TensorShapeProto shape;
-      auto* dim = shape.add_dim();
-      dim->set_size(1);
-      dim = shape.add_dim();
-      dim->set_size(10);
+  void Predict(const std::string& serve_name) {
+    PredictRequest request;
+    auto* model_spec = request.mutable_model_spec();
+    model_spec->set_name(serve_name);
+    model_spec->set_signature_name("predicts");
+    auto* inputs = request.mutable_inputs();
+    TensorProto input_tensor;
+    TensorShapeProto shape;
+    auto* dim = shape.add_dim();
+    dim->set_size(1);
+    dim = shape.add_dim();
+    dim->set_size(10);
 
-      auto* input_shape = input_tensor.mutable_tensor_shape();
-      (*input_shape) = shape;
+    auto* input_shape = input_tensor.mutable_tensor_shape();
+    (*input_shape) = shape;
 
-      input_tensor.set_dtype(DataType::DT_INT64);
-      for (int i = 0; i < 10; i++) {
-        input_tensor.add_int64_val(i);
-      }
-
-      (*inputs)["watched"] = input_tensor;
-
-      PredictResponse response;
-      ClientContext context;
-
-      Status status = stub_->Predict(&context, request, &response);
-      if (status.ok()) {
-        std::cout << "OK" << std::endl;
-        std::cout << response.DebugString() << std::endl;
-      } else {
-        std::cout << status.error_code() << ": " << status.error_message()
-                  << std::endl;
-      }
+    input_tensor.set_dtype(DataType::DT_INT64);
+    for (int i = 0; i < 10; i++) {
+      input_tensor.add_int64_val(i);
     }
 
-  private:
-    std::unique_ptr<PredictionService::Stub> stub_;
+    (*inputs)["watched"] = input_tensor;
+
+    PredictResponse response;
+    ClientContext context;
+
+    Status status = stub_->Predict(&context, request, &response);
+    if (status.ok()) {
+      std::cout << "OK" << std::endl;
+      std::cout << response.DebugString() << std::endl;
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+    }
+  }
+
+ private:
+  std::unique_ptr<PredictionService::Stub> stub_;
 };
 
-int main(int argc, char *argv[]) {
-  PredictionClient client(grpc::CreateChannel(
-        argv[1], grpc::InsecureChannelCredentials()));
-  client.Predict();
+int main(int argc, char* argv[]) {
+  PredictionClient client(
+      grpc::CreateChannel(argv[1], grpc::InsecureChannelCredentials()));
+  client.Predict(argv[2]);
 
   return 0;
 }
