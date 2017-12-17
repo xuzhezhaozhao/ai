73821bf01d7ee3806177df8b771d8854bac2a4cf
zhezhao <zhezhaoxu@gmail.com>
2017-12-17 23:36:08 +0800

    update

--- tags/train.sh
+++ tags/train.sh
@@ -3,16 +3,18 @@
 ft_in=data/tags.in.shuf
 ft_in_raw=data/tags.raw.in.shuf
 
-minCount=1
+minCount=10
 minn=0
 maxn=0
 thread=2
 dim=100
 ws=5
 epoch=5
+neg=5
+lr=0.025
 
 for input in ${ft_in} ${ft_in_raw}; do
 
-utils/fasttext skipgram -input ${input} -output ${input} -lr 0.025 -dim ${dim} -ws ${ws} -epoch ${epoch} -minCount ${minCount} -neg 5 -loss ns -bucket 2000000 -minn ${minn} -maxn ${maxn} -thread ${thread} -t 1e-4 -lrUpdateRate 100  && awk 'NR>2{print $1}' ${input}.vec > ${input}.dict
+utils/fasttext skipgram -input ${input} -output ${input} -lr ${lr} -dim ${dim} -ws ${ws} -epoch ${epoch} -minCount ${minCount} -neg ${neg} -loss ns -bucket 2000000 -minn ${minn} -maxn ${maxn} -thread ${thread} -t 1e-4 -lrUpdateRate 100  && awk 'NR>2{print $1}' ${input}.vec > ${input}.dict
 
 done
