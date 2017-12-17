def9efde69831d313ddbcc331ac1cdf75b01a84f
zhezhao <zhezhaoxu@gmail.com>
2017-12-17 22:54:46 +0800

    update

--- tags/preprocess.sh
+++ tags/preprocess.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env bash
 
-ft_in=data/tags.ft.in
+ft_in=data/tags.in
+ft_in_raw=data/tags.raw.in
 
-python tags.py --input_video_tags_file data/video_tags.csv --input_tag_info_file data/taginfo.csv --input_article_tags_file data/article_tags.csv --output ${ft_in}
+python tags.py --input_video_tags_file data/video_tags.csv --input_tag_info_file data/taginfo.csv --input_article_tags_file data/article_tags.csv --output_info ${ft_in} --output_raw ${ft_in_raw}
+
+shuf -o ${ft_in}.shuf ${ft_in}
+shuf -o ${ft_in_raw}.shuf ${ft_in_raw}
--- tags/train.sh
+++ tags/train.sh
@@ -1,15 +1,18 @@
 #! /usr/bin/env bash
 
-ft_in=data/tags.ft.in
-ft_prefix=data/tags.ft
+ft_in=data/tags.in.shuf
+ft_in_raw=data/tags.raw.in.shuf
 
-minCount=3
+minCount=1
 minn=0
 maxn=0
 thread=2
 dim=100
 ws=5
 epoch=5
-utils/fasttext skipgram -input ${ft_in} -output ${ft_prefix} -lr 0.025 -dim ${dim} -ws ${ws} -epoch ${epoch} -minCount ${minCount} -neg 5 -loss ns -bucket 2000000 -minn ${minn} -maxn ${maxn} -thread ${thread} -t 1e-4 -lrUpdateRate 100
 
-awk 'NR>2{print $1}' tags.ft.vec > ${ft_prefix}.dict
+for input in ${ft_in} ${ft_in_raw}; do
+
+utils/fasttext skipgram -input ${input} -output ${input} -lr 0.025 -dim ${dim} -ws ${ws} -epoch ${epoch} -minCount ${minCount} -neg 5 -loss ns -bucket 2000000 -minn ${minn} -maxn ${maxn} -thread ${thread} -t 1e-4 -lrUpdateRate 100  && awk 'NR>2{print $1}' ${input}.vec > ${input}.dict
+
+done
