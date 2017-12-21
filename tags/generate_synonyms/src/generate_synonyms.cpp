6451f9ecdea425c7cf2ba647f210af94abaa80ea
zhezhaoxu <zhezhaoxu@tencent.com>
2017-12-21 13:13:00 +0800

    update

--- tags/generate_synonyms/src/generate_synonyms.cpp
+++ tags/generate_synonyms/src/generate_synonyms.cpp
@@ -30,6 +30,21 @@ static void printUsage() {
       << std::endl;
 }
 
+// size of predictions must be at least 2
+static std::string generate_key(
+    const std::vector<std::pair<float, std::string>> &predictions) {
+  std::string key;
+  std::string label0 = predictions[0].second.substr(9);
+  std::string label1 = predictions[1].second.substr(9);
+
+  if (predictions[1].first < 0.01) {
+    key = label0;
+  } else {
+    key = std::min(label0, label1) + '#' + std::max(label0, label1);
+  }
+  return key;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 4) {
     printUsage();
@@ -43,22 +58,33 @@ int main(int argc, char *argv[]) {
   std::map<std::string, std::vector<std::string>> tagindex;
   for (auto &tagname : tags) {
     auto predictions = classifier.Predict(tagname, 2, false);
-    std::string key;
-    if (predictions.size() == 2) {
-      key = predictions[0].second + "#" + predictions[1].second;
-    } else if (predictions.size() == 1) {
-      key = predictions[0].second;
-    } else {
-      std::cerr << "no predictions returned, tagname = " << tagname
-                << std::endl;
+    if (predictions.size() < 2) {
       continue;
     }
+    std::string key = generate_key(predictions);
     tagindex[key].push_back(tagname);
   }
 
   std::cout << "tagindex size: " << tagindex.size() << std::endl;
 
   // write tagindex to file
+  std::ofstream ofs(argv[3], std::ios_base::out | std::ios_base::trunc);
+  if (!ofs.is_open()) {
+    std::cerr << "open output file [" << argv[3] << "] failed." << std::endl;
+    exit(-1);
+  }
+
+  for (auto &p : tagindex) {
+    ofs.write(p.first.data(), p.first.size());
+    ofs.write(" ", 1);
+    for (auto &tagname : p.second) {
+      ofs.write("#", 1);
+      ofs.write(tagname.data(), tagname.size());
+    }
+    ofs.write("\n", 1);
+  }
+
+  ofs.close();
 
   return 0;
 }
