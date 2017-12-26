2ca74bcc6362dbbcb771630f3be9bf7b1fd0ca56
zhezhaoxu <zhezhaoxu@tencent.com>
2017-12-26 20:42:40 +0800

    update

--- tar_ynet.sh
+++ tar_ynet.sh
@@ -1,10 +1,4 @@
 tar cvzf ynet.tgz ynet/ \
     --exclude=ynet/model  \
     --exclude=ynet/*.pyc  \
-    --exclude=ynet/data/data.in  \
-    --exclude=ynet/data/data.in.noheader  \
-    --exclude=ynet/data/data.in.noheader.bin  \
-    --exclude=ynet/data/data.in.noheader.preprocessed  \
-    --exclude=ynet/data/data.in.noheader.sorted  \
-    --exclude=ynet/data/data.in.noheader.vec  \
-    --exclude=ynet/data.bak
+    --exclude=ynet/data
