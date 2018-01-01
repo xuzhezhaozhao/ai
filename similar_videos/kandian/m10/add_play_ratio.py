f32e27abea854e037834cf61c534e5451883bf7c
zhezhaoxu <zhezhaoxu@tencent.com>
2018-01-01 11:45:27 +0800

    update

--- similar_videos/kandian/m10/add_play_ratio.py
+++ similar_videos/kandian/m10/add_play_ratio.py
@@ -16,25 +16,33 @@ def main():
         if len(tokens) < 2:
             print("[ratio file] line in {}: {}".format(index, line))
             continue
-        play_ratios[tokens[0]] = tokens[1]
+        play_ratios[tokens[0]] = float(tokens[1])
 
     with open(FLAGS.output_result_file, 'w') as fout:
+        weighted_nn = list()
         for index, line in enumerate(open(FLAGS.input_nn_result_file)):
             tokens = line.strip().split(' ')
             if len(tokens) < 2:
                 print("[nn result file] line in {}: {}".format(index, line))
                 continue
             rowkey = tokens[0]
-            score = tokens[1]
+            score = float(tokens[1])
             if rowkey not in play_ratios:
                 print("[W] rowkey not in ratio file: {}".format(rowkey))
-                ratio = '0.0'
+                ratio = 0.0
             else:
                 ratio = play_ratios[rowkey]
 
-            mean = score * FLAGS.nn_score_weight + \
+            weighted_score = score * FLAGS.nn_score_weight + \
                 ratio * (1 - FLAGS.nn_score_weight)
-            fout.write(rowkey + ' ' + str(mean))
+
+            weighted_nn.append((weighted_score, rowkey))
+            if index != 0 and index % FLAGS.nn_k == 0:
+                for item in weighted_nn:
+                    weighted_score = item[0]
+                    rowkey = item[1]
+                    fout.write(rowkey + ' ' + str(weighted_score) + '\n')
+                weighted_nn[:] = []
 
 
 if __name__ == "__main__":
