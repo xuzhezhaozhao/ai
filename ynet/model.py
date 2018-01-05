1191c0516be73d9001eed0f51796c6e2c6bd2bfa
zhezhaoxu <zhezhaoxu@tencent.com>
2018-01-05 16:44:31 +0800

    update

--- ynet/model.py
+++ ynet/model.py
@@ -5,6 +5,7 @@ from __future__ import division
 
 import tensorflow as tf
 from input_data_binary import load_video_embeddings_from_binary
+import math
 
 
 def model_fn(features, labels, mode, params):
@@ -12,29 +13,51 @@ def model_fn(features, labels, mode, params):
     video_embeddings_dummy, num_videos, embedding_dim = \
         load_video_embeddings_from_binary(params["embeddings_file_path"])
 
-    video_embeddings = tf.Variable(
-        tf.random_uniform(shape=[num_videos, embedding_dim], minval=0,
-                          maxval=1.0/num_videos),
-        trainable=True,
-        name="video_embeddings"
+    embeddings = tf.Variable(
+        tf.random_uniform([num_videos, embedding_dim], -1.0, 1.0),
+        name="embeddings"
     )
+    embed = tf.nn.embedding_lookup(embeddings, features["watched"])
+    mean_input = tf.reduce_mean(embed, 1)
 
-    video_biases = tf.Variable(
-        tf.zeros([num_videos]),
-        trainable=True,
-        name="video_biases"
+    # Construct the variables for the NCE loss
+    nce_weights = tf.Variable(
+        tf.truncated_normal([num_videos, embedding_dim],
+                            stddev=1.0 / math.sqrt(embedding_dim)))
+    nce_biases = tf.Variable(tf.zeros([num_videos]))
+
+    # Compute the average NCE loss for the batch.
+    # tf.nce_loss automatically draws a new sample of the negative labels each
+    # time we evaluate the loss.
+    # Explanation of the meaning of NCE loss:
+    #   http://mccormickml.com/2016/04/19/word2vec-tutorial-the-skip-gram-model/
+    losses = tf.nn.nce_loss(
+        weights=nce_weights,
+        biases=nce_biases,
+        labels=labels,
+        inputs=mean_input,
+        num_sampled=params["num_sampled"],
+        num_classes=num_videos,
+        name="nce_losses"
     )
+    loss = tf.reduce_mean(losses, name="nce_loss_mean")
 
-    x = tf.gather(video_embeddings, features["watched"])
-    mean_input = tf.reduce_mean(x, 1)
+    optimizer = tf.train.AdamOptimizer(learning_rate=params["learning_rate"])
+    trainable_variables = tf.trainable_variables()
+    gradients = optimizer.compute_gradients(loss, trainable_variables)
+
+    tf.summary.scalar('nce_loss', loss)
+    # Add gradients to summary
+    for gradient, var in gradients:
+        tf.summary.histogram(var.name + '/gradient', gradient)
+
+    train_op = optimizer.apply_gradients(
+        grads_and_vars=gradients,
+        global_step=tf.train.get_global_step(),
+    )
 
     # Generate top-K predictions
-    probs = tf.nn.bias_add(
-        tf.matmul(mean_input, video_embeddings, transpose_b=True),
-        video_biases,
-        name="probs"
-    )
-
+    probs = tf.matmul(mean_input, embeddings, transpose_b=True)
     predictions = tf.nn.top_k(probs, params["k"], name="top_k_predictions")
     predictions_k_1 = tf.nn.top_k(probs, 1, name="top_1_predictions")
 
@@ -56,34 +79,6 @@ def model_fn(features, labels, mode, params):
         )
 
     one_hot_labels = tf.reshape(labels, [-1])
-
-    # Calculate loss
-    losses = tf.nn.nce_loss(
-        weights=video_embeddings,
-        biases=video_biases,
-        labels=labels,
-        inputs=mean_input,
-        num_sampled=params["num_sampled"],
-        num_classes=num_videos,
-        remove_accidental_hits=True,
-        name="nce_losses"
-    )
-    loss = tf.reduce_mean(losses, name="nce_loss_mean")
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=params["learning_rate"])
-    trainable_variables = tf.trainable_variables()
-    gradients = optimizer.compute_gradients(loss, trainable_variables)
-
-    tf.summary.scalar('nce_loss', loss)
-
-    # Add gradients to summary
-    for gradient, var in gradients:
-        tf.summary.histogram(var.name + '/gradient', gradient)
-
-    train_op = optimizer.apply_gradients(
-        grads_and_vars=gradients,
-        global_step=tf.train.get_global_step(),
-    )
 
     eval_metric_ops = {
         "accuracy": tf.metrics.accuracy(
