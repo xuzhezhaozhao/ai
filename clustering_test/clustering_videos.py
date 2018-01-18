1f67eaaccc4c2c019696fca0a2393b109061e2cb
zhezhaoxu <zhezhaoxu@tencent.com>
2018-01-18 14:20:11 +0800

    add clustering_videos.py

---
+++ clustering_test/clustering_videos.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+# -*- coding=utf8 -*-
+
+
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import normalize
+import argparse
+import numpy as np
+
+FLAGS = None
+
+
+def main():
+    data = np.loadtxt(FLAGS.input_vec_file,
+                      dtype=np.str,
+                      delimiter=' ',
+                      skiprows=2)
+    D = data[:, :1].ravel()
+    data = data[:, 1:-1].astype(np.float)
+    print('data shape: {}'.format(data.shape))
+    # normalize
+    data = normalize(data, axis=1)
+
+    y = KMeans(n_clusters=FLAGS.ncluster).fit_predict(data)
+    # y = DBSCAN(eps=0.1, metric='euclidean', n_jobs=-1).fit_predict(data)
+
+    num = y.shape[0]
+    clusters = dict()
+    for i in range(num):
+        label = y[i]
+        if label not in clusters:
+            clusters[label] = list()
+        clusters[label].append(D[i])
+
+    with open(FLAGS.output_cluster_file, 'w') as f:
+        f.write(str(len(clusters)))
+        f.write('\n')
+        for label in clusters:
+            f.write(str(len(clusters[label])))
+            f.write(' ')
+        f.write('\n')
+        for label in clusters:
+            f.write('__label__' + str(label) + ' ')
+            for item in clusters[label]:
+                f.write(item + ' ')
+            f.write('\n')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_vec_file',
+        type=str,
+        required=True,
+        help=''
+    )
+
+    parser.add_argument(
+        '--ncluster',
+        type=int,
+        required=True,
+        help=''
+    )
+
+    parser.add_argument(
+        '--output_cluster_file',
+        type=str,
+        required=True,
+        help=''
+    )
+
+    FLAGS, unparsed = parser.parse_known_args()
+    main()
