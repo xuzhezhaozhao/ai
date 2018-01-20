ec87871d54bf30356670ad93ef989806051757b1
zhezhaoxu <zhezhaoxu@tencent.com>
2018-01-20 10:29:12 +0800

    update

--- blas/test.c
+++ blas/test.c
@@ -1,42 +1,46 @@
 
 #include <stdlib.h>
 
-#include "blas/sgemv.h"
+#include "/home/xzz/OpenBLAS/cblas.h"
 
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
 
+typedef float real;
+
 int main(int argc, char *argv[]) {
   int i;
 
   char trans[] = {'N'};  // don't transform
-  integer m = 150000;       // M x N
-  integer n = 100;
+  int m = 250000;       // M x N
+  int n = 100;
   real alpha = 1.0;
   real *a = (real *)malloc(m * n * sizeof(real));
   for (i = 0; i < m * n; ++i) {
     a[i] = i;
   }
 
-  integer lda = m;
+  int lda = m;
   real *x = (real *)malloc(n * sizeof(real));
   for (i = 0; i < n; ++i) {
     x[i] = i;
   }
 
-  integer incx = 1;
+  int incx = 1;
   real beta = 0.0;
   real *y = (real *)malloc(m * sizeof(real));
-  integer incy = 1;
+  int incy = 1;
 
   struct timeval stv;
   gettimeofday(&stv, NULL);
   long long start = stv.tv_sec * 1000 + stv.tv_usec / 1000;
 
-  /*clock_t start = clock();*/
-  int ret = sgemv_(trans, &m, &n, &alpha, a, &lda, x, &incx, &beta, y, &incy);
-  /*clock_t end = clock();*/
+  /*int ret = sgemv_(trans, &m, &n, &alpha, a, &lda, x, &incx, &beta, y, &incy);*/
+  /*openblas_set_num_threads(4);*/
+  /*int nthread = openblas_get_num_threads();*/
+  /*printf("nthread: %d\n", nthread);*/
+  cblas_sgemv(CblasColMajor, CblasNoTrans, m, n, alpha, a, lda, x, incx, beta, y, incy);
 
   gettimeofday(&stv, NULL);
   long long end = stv.tv_sec * 1000 + stv.tv_usec / 1000;
@@ -46,7 +50,6 @@ int main(int argc, char *argv[]) {
     printf("%.2f ", y[i]);
   }
   printf("\n");
-  printf("%d\n", ret);
 
   return 0;
 }
