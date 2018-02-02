136f3b1c9584258b75be1d9416b0c3069174e225
zhezhaoxu <zhezhaoxu@tencent.com>
2018-02-02 15:46:16 +0800

    update

--- dnn/m10/sendupdate_dnn.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -e
-
-MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${MYDIR}
-
-./utils/sendupdate -modid=900545 -cmdid=65536 > log/sendupdate_dnn.log 2>&1 || /data/utils/sendupdate -modid=900545 -cmdid=65536 > log/sendupdate_dnn.log 2>&1
-./utils/sendupdate -modid=934849 -cmdid=65536 > log/sendupdate_dnn.log 2>&1 || /data/utils/sendupdate -modid=934849 -cmdid=65536 > log/sendupdate_dnn.log 2>&1
