4c40d78c41c9a01d09a8c1160e3a2a949b666f7d
zhezhaoxu <zhezhaoxu@tencent.com>
2018-02-07 10:41:03 +0800

    update

--- hbcf/m10/multi_version.sh
+++ hbcf/m10/multi_version.sh
@@ -10,15 +10,22 @@ input=${data_dir}/data.in
 preprocessed=${input}.preprocessed
 fast_model=${preprocessed}.shuf
 nn_k=100
-click_bias=200
-
+click_bias=100
 
 python add_play_ratio.py --input_play_ratio_file ${preprocessed}.play_raito --input_nn_result_file ${fast_model}.result.raw --output_result_file ${fast_model}.result --nn_k ${nn_k} --nn_score_weight 0.3
+# L5
 /data/utils/sendupdate -modid=907457 -cmdid=65536 -data=0
 
+sleep 600
 
-# python add_valid_click.py --input_click_file ${preprocessed}.video_click --input_nn_result_file ${fast_model}.result.raw --output_result_file ${fast_model}.result --nn_k ${nn_k} --nn_score_weight 0.3 --click_bias ${click_bias}
+python add_valid_click.py --input_click_file ${preprocessed}.video_click --input_nn_result_file ${fast_model}.result.raw --output_result_file ${fast_model}.result --nn_k ${nn_k} --nn_score_weight 0.0 --click_bias ${click_bias}
+# experiment L5
+/data/utils/sendupdate -modid=907457 -cmdid=65537 -data=2
 
-#/data/utils/sendupdate -test=true -ip=10.56.60.28 -port=52003 -data=2
-#/data/utils/sendupdate -test=true -ip=10.56.90.138 -port=52003 -data=1
-#/data/utils/sendupdate -test=true -ip=100.108.26.169 -port=52003 -data=3
+
+#/data/utils/sendupdate -test=true -port=52003 -data=0 -ip=10.121.150.27
+#/data/utils/sendupdate -test=true -port=52003 -data=0 -ip=10.56.60.28
+#/data/utils/sendupdate -test=true -port=52003 -data=0 -ip=10.121.151.30
+#/data/utils/sendupdate -test=true -port=52003 -data=0 -ip=10.56.90.138
+#/data/utils/sendupdate -test=true -port=52003 -data=0 -ip=100.108.26.169
+#/data/utils/sendupdate -test=true -port=52003 -data=2 -ip=100.65.94.186
