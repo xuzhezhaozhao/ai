8bea4c4eccc7e88706e456efbc60c0aca978d347
zhezhaoxu <zhezhaoxu@tencent.com>
2018-05-23 13:34:28 +0800

    add client

---
+++ tf/knet/client.py
@@ -0,0 +1,44 @@
+
+from __future__ import print_function
+
+import grpc
+
+import tensorflow as tf
+import tensorflow_serving.apis.predict_pb2 as predict_pb2
+import tensorflow_serving.apis.prediction_service_pb2_grpc as prediction_service_pb2_grpc
+
+
+MODEL_NAME = "knet"
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def run():
+    channel = grpc.insecure_channel('localhost:9000')
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = MODEL_NAME
+
+    request.model_spec.signature_name = 'predicts'
+    input_name = 'examples'
+
+    example1 = tf.train.Example(
+        features=tf.train.Features(
+            feature={
+                'records': _int64_feature([1, 2, 3, 4, 5]),
+            }
+        )
+    ).SerializeToString()
+    examples = [example1]
+
+    request.inputs[input_name].CopyFrom(
+        tf.contrib.util.make_tensor_proto(examples, dtype=tf.string))
+
+    response = stub.Predict(request)
+    print("Received: {}".format(response))
+
+
+if __name__ == '__main__':
+    run()
---
+++ tf/knet/run_client.sh
@@ -0,0 +1,6 @@
+
+cd protos
+bash gen.sh
+
+cd python_out
+python client.py
