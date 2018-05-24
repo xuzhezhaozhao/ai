8d30d250b1031d95b61de09e623783bb94333d6f
zhezhaoxu <zhezhaoxu@tencent.com>
2018-05-24 22:11:34 +0800

    update

--- tf/ops/ftlib/fasttext_example_generate_kernels.cc
+++ tf/ops/ftlib/fasttext_example_generate_kernels.cc
@@ -7,6 +7,7 @@
 #include "tensorflow/core/lib/random/philox_random.h"
 #include "tensorflow/core/lib/random/simple_philox.h"
 #include "tensorflow/core/lib/strings/str_util.h"
+#include "tensorflow/core/platform/posix/posix_file_system.h"
 #include "tensorflow/core/platform/thread_annotations.h"
 #include "tensorflow/core/util/guarded_philox_random.h"
 
@@ -44,63 +45,6 @@ class FasttextExampleGenerateOp : public OpKernel {
       LoadDictionary(ctx);
     }
   }
-
-  void ParseArgs(OpKernelConstruction* ctx) {
-    OP_REQUIRES_OK(ctx,
-                   ctx->GetAttr("train_data_path", &args_->train_data_path));
-    LOG(INFO) << "train_data_path: " << args_->train_data_path;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("dim", &args_->dim));
-    LOG(INFO) << "dim: " << args_->dim;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("maxn", &args_->maxn));
-    LOG(INFO) << "maxn: " << args_->maxn;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("minn", &args_->minn));
-    LOG(INFO) << "minn: " << args_->minn;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("word_ngrams", &args_->word_ngrams));
-    LOG(INFO) << "word_ngrams: " << args_->word_ngrams;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("bucket", &args_->bucket));
-    LOG(INFO) << "bucket: " << args_->bucket;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("ws", &args_->ws));
-    LOG(INFO) << "ws: " << args_->ws;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("min_count", &args_->min_count));
-    LOG(INFO) << "min_count: " << args_->min_count;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("t", &args_->t));
-    LOG(INFO) << "t: " << args_->t;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("verbose", &args_->verbose));
-    LOG(INFO) << "verbose: " << args_->verbose;
-
-    OP_REQUIRES_OK(ctx,
-                   ctx->GetAttr("min_count_label", &args_->min_count_label));
-    LOG(INFO) << "min_count_label: " << args_->min_count_label;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("label", &args_->label));
-    LOG(INFO) << "label: " << args_->label;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("first_run", &args_->first_run));
-    LOG(INFO) << "first_run: " << args_->first_run;
-
-    OP_REQUIRES_OK(ctx, ctx->GetAttr("dict_dir", &args_->dict_dir));
-    LOG(INFO) << "dict_dir: " << args_->dict_dir;
-  }
-
-  void PreProcessTrainData(OpKernelConstruction* ctx) {
-    LOG(INFO) << "Preprocess train data beginning ...";
-    std::ifstream ifs(args_->train_data_path);
-    OP_REQUIRES(ctx, ifs.is_open(), errors::Unavailable("File open failed."));
-    dict_->readFromFile(ifs);
-    ifs.close();
-    LOG(INFO) << "Preprocess train data done.";
-  }
-
-  inline int transform_id(int id) { return id + 1; }
 
   void Compute(OpKernelContext* ctx) override {
     ++global_lines_;
@@ -168,8 +112,74 @@ class FasttextExampleGenerateOp : public OpKernel {
     }
   }
 
+ private:
+  void ParseArgs(OpKernelConstruction* ctx) {
+    OP_REQUIRES_OK(ctx,
+                   ctx->GetAttr("train_data_path", &args_->train_data_path));
+    LOG(INFO) << "train_data_path: " << args_->train_data_path;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("dim", &args_->dim));
+    LOG(INFO) << "dim: " << args_->dim;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("maxn", &args_->maxn));
+    LOG(INFO) << "maxn: " << args_->maxn;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("minn", &args_->minn));
+    LOG(INFO) << "minn: " << args_->minn;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("word_ngrams", &args_->word_ngrams));
+    LOG(INFO) << "word_ngrams: " << args_->word_ngrams;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("bucket", &args_->bucket));
+    LOG(INFO) << "bucket: " << args_->bucket;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("ws", &args_->ws));
+    LOG(INFO) << "ws: " << args_->ws;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("min_count", &args_->min_count));
+    LOG(INFO) << "min_count: " << args_->min_count;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("t", &args_->t));
+    LOG(INFO) << "t: " << args_->t;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("verbose", &args_->verbose));
+    LOG(INFO) << "verbose: " << args_->verbose;
+
+    OP_REQUIRES_OK(ctx,
+                   ctx->GetAttr("min_count_label", &args_->min_count_label));
+    LOG(INFO) << "min_count_label: " << args_->min_count_label;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("label", &args_->label));
+    LOG(INFO) << "label: " << args_->label;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("first_run", &args_->first_run));
+    LOG(INFO) << "first_run: " << args_->first_run;
+
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("dict_dir", &args_->dict_dir));
+    LOG(INFO) << "dict_dir: " << args_->dict_dir;
+  }
+
+  void PreProcessTrainData(OpKernelConstruction* ctx) {
+    LOG(INFO) << "Preprocess train data beginning ...";
+    std::ifstream ifs(args_->train_data_path);
+    OP_REQUIRES(ctx, ifs.is_open(), errors::Unavailable("File open failed."));
+    dict_->readFromFile(ifs);
+    ifs.close();
+    LOG(INFO) << "Preprocess train data done.";
+  }
+
+  inline int transform_id(int id) { return id + 1; }
+
   void SaveDictionary(OpKernelConstruction* ctx) {
     auto root_dir = args_->dict_dir;
+
+    auto file_system = ::tensorflow::PosixFileSystem();
+    if (!file_system.FileExists(root_dir)) {
+      auto status = file_system.CreateDir(root_dir);
+      OP_REQUIRES(ctx, status == Status::OK(),
+                  errors::Unavailable("Create dir failed."));
+    }
+
     LOG(INFO) << "SaveDictionary to " << root_dir << " ...";
     auto saved_dict = ::tensorflow::io::JoinPath(root_dir, SAVED_DICT);
     auto dict_meta = ::tensorflow::io::JoinPath(root_dir, DICT_META);
@@ -207,7 +217,7 @@ class FasttextExampleGenerateOp : public OpKernel {
       std::ofstream ofs(dict_words);
       LOG(INFO) << "Write dict words to " << dict_words << " ...";
       OP_REQUIRES(ctx, ofs.is_open(), errors::Unavailable("file open failed"));
-      for (const auto &entry : dict_->words()) {
+      for (const auto& entry : dict_->words()) {
         if (entry.type == ::fasttext::entry_type::word) {
           ofs.write(entry.word.data(), entry.word.size());
           ofs.write("\n", 1);
@@ -229,7 +239,6 @@ class FasttextExampleGenerateOp : public OpKernel {
     LOG(INFO) << "Load dictionary OK";
   }
 
- private:
   const int PADDING_INDEX = 0;
   const std::string SAVED_DICT = "saved_dict.bin";
   const std::string DICT_META = "dict_meta";
