eca2d42942f3e10543c6ea39ef38343b5012cad7
zhezhaoxu <zhezhaoxu@tencent.com>
2018-05-25 15:50:41 +0800

    fix run client

--- tf/knet/client.py
+++ tf/knet/client.py
@@ -1,7 +1,4 @@
-#! /usr/bin/env python
-# -*- coding=utf8 -*-
-
-# from __future__ import absolute_import
+from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
--- tf/knet/run_client.sh
+++ tf/knet/run_client.sh
@@ -3,4 +3,6 @@ cd protos
 bash gen.sh
 
 cd python_out
+rm -rf client.py
+cp ../../client.py .
 python client.py
