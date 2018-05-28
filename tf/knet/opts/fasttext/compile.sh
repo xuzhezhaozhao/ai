bbf14275cd913c55adc63805580c1ac6f54cf823
zhezhaoxu <zhezhaoxu@tencent.com>
2018-05-28 11:54:58 +0800

    update

--- tf/knet/opts/fasttext/compile.sh
+++ tf/knet/opts/fasttext/compile.sh
@@ -2,10 +2,13 @@
 
 TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
 TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
+echo "TF_CFLAGS = " ${TF_CFLAGS[@]}
+echo "TF_LFLAGS = " ${TF_LFLAGS[@]}
 echo "compile fasttext_example_generate_ops ..."
+
 g++ -std=c++11 -shared \
     -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} \
-    -O2 -D_GLIBCXX_USE_CXX11_ABI=1 \
+    -O2 \
     fasttext_example_generate_ops.cc \
     fasttext_example_generate_kernels.cc \
     dictionary.cc \
