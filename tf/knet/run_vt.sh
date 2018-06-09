06eb4c36511be0b0d3f19cc51281dc20370721a6
zhezhaoxu <zhezhaoxu@tencent.com>
2018-06-09 18:39:50 +0800

    update

--- tf/knet/run_vt.sh
+++ tf/knet/run_vt.sh
@@ -9,7 +9,7 @@ ts=`date +%Y%m%d%H%M%S`
 vt_log=log/vt.log.${ts}
 
 # train with only video history
-(./fetchdata_vt.sh && ./preprocess_vt.sh) > ${vt_log} 2>&1
+# (./fetchdata_vt.sh && ./preprocess_vt.sh) > ${vt_log} 2>&1
 
 # train with video and article history
-# (./fetchdata_vt_all.sh && ./preprocess_vt.sh) > ${vt_log} 2>&1
+(./fetchdata_vt_all.sh && ./preprocess_vt.sh) > ${vt_log} 2>&1
