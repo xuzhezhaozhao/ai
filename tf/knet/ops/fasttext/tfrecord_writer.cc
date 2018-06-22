2d93f4d1b9108f13c4e8ad6fdd9e0cdbb03248d7
zhezhaoxu <zhezhaoxu@tencent.com>
2018-06-22 21:20:17 +0800

    add tfrecord writer

---
+++ tf/knet/ops/fasttext/tfrecord_writer.cc
@@ -0,0 +1,45 @@
+
+#include "tensorflow/core/lib/io/record_writer.h"
+#include "tensorflow/core/example/example.pb.h"
+#include "tensorflow/core/platform/env.h"
+
+#include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/lib/core/status.h"
+#include "tensorflow/core/lib/strings/strcat.h"
+#include "tensorflow/core/platform/logging.h"
+
+class WritableFile;
+
+int main(int argc, char *argv[]) {
+  // Create Example
+  ::tensorflow::Example example;
+  auto features = example.mutable_features();
+  auto feature = features->mutable_feature();
+  ::tensorflow::Feature words;
+  auto bytes_list = words.mutable_bytes_list();
+  int ws = 20;
+  for (int i = 0; i < ws; ++i) {
+    bytes_list->add_value("a");
+  }
+  for (int i = bytes_list->value_size(); i < ws; ++i) {
+    bytes_list->add_value("");
+  }
+  (*feature)["words"] = words;
+
+  std::string serialized;
+  example.SerializeToString(&serialized);
+
+
+  ::tensorflow::Env* env = ::tensorflow::Env::Default();
+  std::string fname = "example.tfrecord";
+  int buf_size = 2048;
+
+  std::unique_ptr<::tensorflow::WritableFile> file;
+  TF_CHECK_OK(env->NewWritableFile(fname, &file));
+  ::tensorflow::io::RecordWriterOptions options;
+  options.zlib_options.output_buffer_size = buf_size;
+  ::tensorflow::io::RecordWriter writer(file.get(), options);
+  TF_CHECK_OK(writer.Flush());
+
+  return 0;
+}
