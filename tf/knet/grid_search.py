1bc7be860777b83f593d4711443aa24310e3c8e2
zhezhaoxu <zhezhaoxu@tencent.com>
2018-06-27 01:33:17 +0800

    update

---
+++ tf/knet/grid_search.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding=utf8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+param_lr =
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
