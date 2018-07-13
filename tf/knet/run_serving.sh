629a317c0970716a085644dddbd8167345b396c4
zhezhaoxu <zhezhaoxu@tencent.com>
2018-07-13 17:34:21 +0800

    update

--- tf/knet/run_serving.sh
+++ tf/knet/run_serving.sh
@@ -1,4 +1,4 @@
 
 killall -9 tensorflow_model_server > /dev/null 2>&1
 sleep 2
-./utils/tensorflow_model_server --model_name="knet" --port=9000 --model_base_path=$(pwd)/export_model_dir > serving.log 2>&1 &
+tensorflow_model_server --model_name="knet" --port=9000 --model_base_path=$(pwd)/export_model_dir > serving.log 2>&1 &
