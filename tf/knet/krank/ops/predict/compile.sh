b26ab6439fea3ff7935b42cf77fb255b841363b5
zhezhaoxu <zhezhaoxu@tencent.com>
2018-09-22 14:51:07 +0800

    add BUILD

--- tf/knet/krank/ops/predict/compile.sh
+++ tf/knet/krank/ops/predict/compile.sh
@@ -9,7 +9,7 @@ echo "compile krank_predict_input_ops.so ..."
 g++ -std=c++11 -shared \
     krank_predict_input_kernels.cc \
     krank_predict_input_ops.cc \
-    ../../fe/stringpiece.cpp \
-    ../../fe/str_util.cpp \
+    fe/stringpiece.cpp \
+    fe/str_util.cpp \
     -o krank_predict_input_ops.so \
     -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2
