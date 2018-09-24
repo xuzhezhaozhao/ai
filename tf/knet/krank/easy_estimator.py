16e0095a52aabd384e17b6fcd187b300b29c9e09
zhezhaoxu <zhezhaoxu@tencent.com>
2018-09-24 13:25:01 +0800

    building easy_estimator

--- tf/knet/krank/easy_estimator.py
+++ tf/knet/krank/easy_estimator.py
@@ -7,3 +7,113 @@ from __future__ import print_function
 
 
 import tensorflow as tf
+import copy
+import os
+from datetime import datetime
+
+
+class EasyEstimator(object):
+    def __init__(self, model_fn, model_dir=None, config=None, params=None):
+
+        if config is None:
+            self._config = tf.estimator.RunConfig()
+            tf.logging.info('Using default config.')
+        else:
+            if not isinstance(config, tf.estimator.RunConfig):
+                raise ValueError('config must be an instance of RunConfig, '
+                                 'but provided %s.' % config)
+        self._config = config
+
+        self._model_dir = model_dir or self._config.model_dir
+        if self._model_dir is None:
+            raise ValueError("model_dir should not be None.")
+        if self._config.model_dir is None:
+            self._config = self._config.replace(model_dir=self._model_dir)
+
+        tf.logging.info('Using config: %s', str(vars(self._config)))
+
+        self._model_fn = model_fn
+        self._params = copy.deepcopy(params or {})
+
+    @property
+    def model_dir(self):
+        return self._model_dir
+
+    @property
+    def params(self):
+        return copy.deepcopy(self._params)
+
+    def train(self,
+              input_fn,
+              hooks=None,
+              steps=None,
+              max_steps=None):
+
+        loss = self._train_model(input_fn, hooks)
+        tf.logging.info('Loss for final step: %s.', loss)
+        return self
+
+    def _train_model(self, input_fn, hooks):
+        with tf.Graph().as_default() as g:
+            tf.set_random_seed(self._config.tf_random_seed)
+            global_step_tensor = tf.train.get_or_create_global_step(g)
+            features, labels = self._get_features_and_labels_from_input_fn(
+                input_fn, tf.estimator.ModeKeys.TRAIN)
+            estimator_spec = self._call_model_fn(
+                features, labels, tf.estimator.ModeKeys.TRAIN)
+            self._merged_summary = tf.summary.merge_all()
+            self._writer = tf.summary.FileWriter(self._model_dir)
+            self._saver = tf.train.Saver()
+
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+                sess.run(self._iterator_initializer)
+                self._writer.add_graph(sess.graph)
+
+                lastest_path = tf.train.latest_checkpoint(self._model_dir)
+                if lastest_path is not None:
+                    tf.logging.info('restore model ckpt from {} ...'
+                                    .format(lastest_path))
+                    self._saver.restore(sess, lastest_path)
+
+                tf.logging.info('{} Start training ...'.format(datetime.now()))
+
+                _, loss = sess.run([estimator_spec.train_op,
+                                    estimator_spec.loss])
+
+                global_steps = sess.run(global_step_tensor)
+                tf.logging.info('global_steps = {}'.format(global_steps))
+                ckpt_name = os.path.join(self._model_dir,
+                                        'model.ckpt-{}'.format(global_steps))
+                ckpt_path = self._saver.save(sess, ckpt_name)
+                tf.logging.info('{} Model ckpt saved at {}'
+                                .format(datetime.now(), ckpt_path))
+
+            return loss
+
+    def _get_features_and_labels_from_input_fn(self, input_fn, mode):
+        result = self._call_input_fn(input_fn, mode)
+
+        return self._parse_input_fn_result(result)
+
+    def _call_input_fn(self, input_fn, mode):
+        with tf.device('/cpu:0'):
+            return input_fn()
+
+    def _parse_input_fn_result(self, result):
+        iterator = result.make_initializable_iterator()
+        self._iterator_initializer = iterator.initializer
+        data = iterator.get_next()
+
+        return data[0], data[1]
+
+    def _call_model_fn(self, features, labels, mode):
+        tf.logging.info('Calling model_fn.')
+        model_fn_results = self._model_fn(
+            features=features, labels=labels, params=self.params, mode=mode)
+        tf.logging.info('Done calling model_fn.')
+
+        if not isinstance(model_fn_results, tf.estimator.EstimatorSpec):
+            raise ValueError('model_fn should return an EstimatorSpec.')
+
+        return model_fn_results
--- tf/knet/krank/train.py
+++ tf/knet/krank/train.py
@@ -12,17 +12,12 @@ import build_model_fn
 import hook
 import input_data
 import model_keys
+import easy_estimator
 
 
 def build_estimator(opts):
     """Build estimator."""
 
-    """ session config
-    session_config = tf.ConfigProto(device_count={"CPU": 1},
-                                    inter_op_parallelism_threads=1,
-                                    intra_op_parallelism_threads=1,
-                                    log_device_placement=False)
-    """
     config_keys = {}
     config_keys['model_dir'] = opts.model_dir
     config_keys['tf_random_seed'] = None
@@ -32,8 +27,10 @@ def build_estimator(opts):
     config_keys['keep_checkpoint_max'] = opts.keep_checkpoint_max
     config_keys['keep_checkpoint_every_n_hours'] = 10000
     config_keys['log_step_count_steps'] = opts.log_step_count_steps
+    config = tf.estimator.RunConfig(**config_keys)
 
     estimator_keys = {}
+    estimator_keys['model_dir'] = opts.model_dir
     estimator_keys['model_fn'] = build_model_fn.krank_model_fn
     (positive_records_col,
      negative_records_col,
@@ -44,96 +41,30 @@ def build_estimator(opts):
         model_keys.TARGETS_COL: targets_col,
         'opts': opts
     }
-
-    train_parallel_mode = opts.train_parallel_mode
-    if train_parallel_mode == model_keys.TrainParallelMode.DEFAULT:
-        config = tf.estimator.RunConfig(**config_keys)
-        estimator_keys['config'] = config
-        estimator = tf.estimator.Estimator(**estimator_keys)
-    elif train_parallel_mode == model_keys.TrainParallelMode.TRAIN_OP_PARALLEL:
-        from estimator.estimator import TrainOpParallelEstimator
-        config = tf.estimator.RunConfig(**config_keys)
-        estimator_keys['config'] = config
-        estimator_keys['num_train_op_parallel'] = opts.train_num_parallel
-        estimator = TrainOpParallelEstimator(**estimator_keys)
-    elif train_parallel_mode == model_keys.TrainParallelMode.MULTI_THREAD:
-        from estimator_v2.estimator import Estimator as MultiThreadEstimator
-        config = tf.estimator.RunConfig(**config_keys)
-        estimator_keys['config'] = config
-        estimator_keys['num_thread'] = opts.train_num_parallel
-        estimator = MultiThreadEstimator(**estimator_keys)
-    elif train_parallel_mode == model_keys.TrainParallelMode.MULTI_THREAD_V2:
-        from estimator_v3.estimator import TrainOpParallelEstimator
-        config = tf.estimator.RunConfig(**config_keys)
-        estimator_keys['config'] = config
-        estimator_keys['num_train_op_parallel'] = opts.train_num_parallel
-        estimator = TrainOpParallelEstimator(**estimator_keys)
-    else:
-        raise ValueError("train_parallel_mode '{}' not surpported.".format(
-            train_parallel_mode))
+    estimator_keys['config'] = config
+    estimator = easy_estimator.EasyEstimator(**estimator_keys)
 
     return estimator
-
-
-def create_hooks(opts):
-    """Create profile hooks."""
-
-    save_steps = opts.profile_steps
-    meta_hook = hook.MetadataHook(save_steps=save_steps,
-                                  output_dir=opts.model_dir)
-    profile_hook = tf.train.ProfilerHook(save_steps=save_steps,
-                                         output_dir=opts.model_dir,
-                                         show_dataflow=True,
-                                         show_memory=True)
-    early_stopping = tf.contrib.estimator.stop_if_no_decrease_hook(
-        opts.estimator,
-        metric_name='loss',
-        max_steps_without_decrease=1000,
-        min_steps=1000)
-
-    hooks = []
-    if opts.use_profile_hook:
-        hooks.append(meta_hook, profile_hook)
-
-    if opts.use_early_stopping:
-        hooks.append(early_stopping)
-
-    return hooks if len(hooks) > 0 else None
 
 
 def train_and_eval_in_local_mode(opts):
     """Train and eval model in lcoal mode."""
 
-    train_spec = tf.estimator.TrainSpec(input_data.input_fn(opts, False, 1),
-                                        max_steps=opts.max_train_steps,
-                                        hooks=opts.hooks)
-    eval_spec = tf.estimator.EvalSpec(
-        input_data.input_fn(opts, True),
-        steps=None,
-        name='Validation',
-        start_delay_secs=opts.early_stopping_start_delay_secs,
-        throttle_secs=opts.early_stopping_throttle_secs)
-    result = tf.estimator.train_and_evaluate(
-        opts.estimator,
-        train_spec,
-        eval_spec)
+    tf.logging.info("Training model ...")
+    build_model_fn.clear_model_fn_times()
 
-    # for epoch in range(opts.epoch):
-        # tf.logging.info("Training model, epoch {} ...".format(epoch+1))
-        # build_model_fn.clear_model_fn_times()
-        # opts.estimator.train(input_fn=input_data.input_fn(opts, False, 1),
-                             # max_steps=opts.max_train_steps,
-                             # hooks=opts.hooks)
-        # tf.logging.info("Train model OK")
+    opts.estimator.train(input_fn=input_data.input_fn(opts, False, 1),
+                         max_steps=opts.max_train_steps)
+    tf.logging.info("Train model OK")
 
-        # # evaluate model
-        # tf.logging.info("Evaluating model, epoch {} ...".format(epoch+1))
-        # result = opts.estimator.evaluate(
-            # input_fn=input_data.input_fn(opts, True),
-            # hooks=opts.hooks)
-        # tf.logging.info("Evaluate model OK")
+    # evaluate model
+    # tf.logging.info("Evaluating model ...")
+    # result = opts.estimator.evaluate(
+        # input_fn=input_data.input_fn(opts, True),
+        # hooks=opts.hooks)
+    # tf.logging.info("Evaluate model OK")
 
-    return result
+    return None
 
 
 def export_model_in_local_mode(opts):
@@ -151,12 +82,10 @@ def train(opts, export=True):
     """Train model."""
 
     opts.estimator = build_estimator(opts)
-    opts.hooks = create_hooks(opts)
-
     opts.list_all_member()
 
     result = train_and_eval_in_local_mode(opts)
     if export:
-        export_model_in_local_mode(opts)
+        # export_model_in_local_mode(opts)
         pass
     return result
