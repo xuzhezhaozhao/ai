ef74e219257438037e49cae44f232c7abd863b11
zhezhaoxu <zhezhaoxu@tencent.com>
2018-10-14 13:46:25 +0800

    krecall fix opts log

--- tf/knet/krecall/train.py
+++ tf/knet/krecall/train.py
@@ -251,8 +251,7 @@ def train(opts, export=True):
 
     opts.estimator = build_estimator(opts)
     opts.hooks = create_hooks(opts)
-
-    tf.logging.info(opts)
+    opts.list_all_member()
 
     if is_distributed(opts):
         train_and_eval_in_distributed_mode(opts)
