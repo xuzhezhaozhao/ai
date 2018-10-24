9fea9305431cd050a1ae3c657558fe0c86a701a0
zhezhaoxu <zhezhaoxu@tencent.com>
2018-10-24 15:08:42 +0800

    update

--- tf/knet/krank/build_model_fn.py
+++ tf/knet/krank/build_model_fn.py
@@ -61,9 +61,9 @@ def krank_model_fn(features, labels, mode, params):
     tf.summary.histogram('first_videos_embeds', first_videos_embeds)
 
     concat_features = [
-        #positive_embeds_mean,
-        #negative_embeds_mean,
-        first_videos_embeds,
+        positive_embeds_mean,
+        negative_embeds_mean,
+        # first_videos_embeds,
         targets_embeds
     ]
 
