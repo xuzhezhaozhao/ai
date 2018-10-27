aff462d64de9000477c5068efa77f6d762d23061
zhezhaoxu <zhezhaoxu@tencent.com>
2018-10-27 14:40:43 +0800

    [vgg] update fe

--- tf/models/vgg/feature_extractor/show_feature_map.py
+++ tf/models/vgg/feature_extractor/show_feature_map.py
@@ -19,20 +19,21 @@ import vgg19
 parser = argparse.ArgumentParser()
 parser.add_argument('--img_path', default='', type=str, help='')
 parser.add_argument('--subplot', default=0, type=int, help='')
+parser.add_argument('--feature_map', default=0, type=int, help='')
 
 
 INPUT_SHAPE = [224, 224]
 VGG_MEAN = [103.939, 116.779, 123.68]
 LAYERS = (
-    'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
-    'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
-    'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
-    'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
-    'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
-    'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
-    'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
-    'relu5_3', 'conv5_4', 'relu5_4', 'pool5'
-    )
+    ['conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1'],
+    ['conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2'],
+    ['conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
+     'relu3_3', 'conv3_4', 'relu3_4', 'pool3'],
+    ['conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
+     'relu4_3', 'conv4_4', 'relu4_4', 'pool4'],
+    ['conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
+     'relu5_3', 'conv5_4', 'relu5_4', 'pool5']
+)
 
 
 def parse_img(img_path):
@@ -60,19 +61,18 @@ def run(args):
         if args.subplot:
             hspace = 0.6
             plt.subplots_adjust(hspace=hspace)
-            for index, layer in enumerate(LAYERS):
-                features = sess.run(vgg.__dict__[layer])
-                print("layer '{}', shape '{}'"
-                      .format(layer, str(features.shape)))
-                plt.subplot(6, 7, index+1)
-                # plt.matshow(features[0, :, :, 0], cmap=plt.cm.gray, fignum=0)
-                # im = plt.imshow(features[0, :, :, 0], cmap=plt.cm.hot)
-                im = plt.imshow(features[0, :, :, 0], cmap=plt.cm.gray)
-                # im = plt.imshow(features[0, :, :, 0], cmap=plt.cm.spring)
-                # im = plt.imshow(features[0, :, :, 0], cmap=plt.cm.cool)
-                # im = plt.imshow(features[0, :, :, 0], cmap=plt.cm.bone)
-                plt.title(layer, y=1.0)
-                plt.colorbar()
+            for layer_index, layer in enumerate(LAYERS):
+                for sublayer_index, sublayer in enumerate(layer):
+                    features = sess.run(vgg.__dict__[sublayer])
+                    print("layer '{}', shape '{}'"
+                          .format(sublayer, str(features.shape)))
+                    plt.subplot(5, 9, (layer_index*9 + sublayer_index)+1)
+                    # cmap: hot, spring, cool, bone
+                    # or use plt.matshow
+                    im = plt.imshow(features[0, :, :, args.feature_map],
+                                    cmap=plt.cm.gray)
+                    plt.title(sublayer, y=1.0)
+                    plt.colorbar(im)
             plt.show()
         else:
             for index, layer in enumerate(LAYERS):
