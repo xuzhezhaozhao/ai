c4e36f78f73b56d42d718fe2778efd40a6b53e0f
zhezhaoxu <zhezhaoxu@tencent.com>
2018-10-27 12:58:34 +0800

    add more examples

--- tf/models/vgg/feature_extractor/show_feature_map.sh
+++ tf/models/vgg/feature_extractor/show_feature_map.sh
@@ -6,3 +6,7 @@ python show_feature_map.py \
 python show_feature_map.py \
     --img_path /home/xzz/ai/datasets/leg-dataset/test/leg_120.jpg \
     --subplot 1 &
+
+python show_feature_map.py \
+    --img_path /home/xzz/ai/datasets/leg-dataset/test/ugc_70.jpg \
+    --subplot 1 &
