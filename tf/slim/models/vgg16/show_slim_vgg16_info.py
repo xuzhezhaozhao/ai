00f488440957aa03729b6217d0333ae51e2e0554
zhezhaoxu <zhezhaoxu@tencent.com>
2018-11-01 15:04:30 +0800

    [slim-vgg16] add show_slim_vgg16_info.py

---
+++ tf/slim/models/vgg16/show_slim_vgg16_info.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import tensorflow.contrib.slim.nets as nets
+
+slim = tf.contrib.slim
+vgg = nets.vgg
+
+inputs = tf.placeholder(tf.float32, shape=(None, 224, 224, 3))
+
+with tf.Graph().as_default():
+    predictions, endpoints = vgg.vgg_16(inputs, is_training=True)
+
+    print('predictions: ')
+    print(predictions)
+    print('\nNetword endpoints: ')
+    for name in endpoints:
+        print('{}: {}'.format(name, endpoints[name]))
