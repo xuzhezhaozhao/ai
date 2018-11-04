add moving average and fix predict bug

tf/slim/image_models/finetune/args_parser.py
@@ -100,9 +100,6 @@ tf.app.flags.DEFINE_float(
     'The minimal end learning rate used by a polynomial decay learning rate.')
 
 tf.app.flags.DEFINE_float(
-    'label_smoothing', 0.0, 'The amount of label smoothing.')
-
-tf.app.flags.DEFINE_float(
     'learning_rate_decay_factor', 0.94, 'Learning rate decay factor.')
 
 tf.app.flags.DEFINE_float(
@@ -111,6 +108,18 @@ tf.app.flags.DEFINE_float(
     ' epochs per clone but aggregates per sync replicas. So 1.0 means that '
     'each clone will go over full epoch individually, but replicas will go '
     'once across all replicas.')
+
+tf.app.flags.DEFINE_float(
+    'label_smoothing', 0.0, 'The amount of label smoothing.')
+
+# moving average flags
+tf.app.flags.DEFINE_bool(
+    'use_moving_average', False,
+    'Weather to use moving average.')
+
+tf.app.flags.DEFINE_float(
+    'moving_average_decay', 0.9,
+    'The decay to use for the moving average.')
 
 # preprocess flags
 tf.app.flags.DEFINE_integer('inference_image_size', 256, '')

tf/slim/image_models/finetune/build_model_fn.py
@@ -15,122 +15,6 @@ import model_keys
 import build_model_fn_utils
 
 
-def get_model(model_name, inputs, is_training, opts):
-    if model_name == 'resnet_v1_50':
-        with slim.arg_scope(resnet_v1.resnet_arg_scope(
-                weight_decay=opts.weight_decay,
-                batch_norm_decay=opts.batch_norm_decay,
-                batch_norm_epsilon=opts.batch_norm_epsilon,
-                activation_fn=tf.nn.relu)):
-            return resnet_v1.resnet_v1_50(inputs,
-                                          num_classes=opts.num_classes,
-                                          is_training=is_training,
-                                          global_pool=opts.global_pool,
-                                          reuse=None)
-    elif model_name == 'inception_v1':
-        with slim.arg_scope(inception.inception_v1_arg_scope(
-                weight_decay=opts.weight_decay,
-                use_batch_norm=opts.use_batch_norm,
-                batch_norm_decay=opts.batch_norm_decay,
-                batch_norm_epsilon=opts.batch_norm_epsilon,
-                activation_fn=tf.nn.relu)):
-            return inception.inception_v1(
-                inputs,
-                num_classes=opts.num_classes,
-                is_training=is_training,
-                dropout_keep_prob=opts.dropout_keep_prob,
-                prediction_fn=slim.softmax,
-                spatial_squeeze=opts.spatial_squeeze,
-                reuse=None,
-                global_pool=opts.global_pool)
-    elif model_name == 'inception_v2':
-        with slim.arg_scope(inception.inception_v2_arg_scope(
-                weight_decay=opts.weight_decay,
-                use_batch_norm=opts.use_batch_norm,
-                batch_norm_decay=opts.batch_norm_decay,
-                batch_norm_epsilon=opts.batch_norm_epsilon,
-                activation_fn=tf.nn.relu)):
-            return inception.inception_v2(
-                inputs,
-                num_classes=opts.num_classes,
-                is_training=is_training,
-                dropout_keep_prob=opts.dropout_keep_prob,
-                min_depth=opts.min_depth,
-                depth_multiplier=opts.depth_multiplier,
-                prediction_fn=slim.softmax,
-                spatial_squeeze=opts.spatial_squeeze,
-                reuse=None,
-                global_pool=opts.global_pool)
-    elif model_name == 'inception_v3':
-        with slim.arg_scope(inception.inception_v3_arg_scope(
-                weight_decay=opts.weight_decay,
-                use_batch_norm=opts.use_batch_norm,
-                batch_norm_decay=opts.batch_norm_decay,
-                batch_norm_epsilon=opts.batch_norm_epsilon,
-                activation_fn=tf.nn.relu)):
-            return inception.inception_v3(
-                inputs,
-                num_classes=opts.num_classes,
-                is_training=is_training,
-                dropout_keep_prob=opts.dropout_keep_prob,
-                min_depth=opts.min_depth,
-                depth_multiplier=opts.depth_multiplier,
-                prediction_fn=slim.softmax,
-                spatial_squeeze=opts.spatial_squeeze,
-                reuse=None,
-                create_aux_logits=opts.create_aux_logits,
-                global_pool=opts.global_pool)
-    elif model_name == 'inception_v4':
-        with slim.arg_scope(inception.inception_v4_arg_scope(
-                weight_decay=opts.weight_decay,
-                use_batch_norm=opts.use_batch_norm,
-                batch_norm_decay=opts.batch_norm_decay,
-                batch_norm_epsilon=opts.batch_norm_epsilon,
-                activation_fn=tf.nn.relu)):
-            return inception.inception_v4(
-                inputs,
-                num_classes=opts.num_classes,
-                is_training=is_training,
-                dropout_keep_prob=opts.dropout_keep_prob,
-                reuse=None,
-                create_aux_logits=opts.create_aux_logits)
-    elif model_name == 'inception_resnet_v2':
-        with slim.arg_scope(inception.inception_resnet_v2_arg_scope(
-                weight_decay=opts.weight_decay,
-                batch_norm_decay=opts.batch_norm_decay,
-                batch_norm_epsilon=opts.batch_norm_epsilon,
-                activation_fn=tf.nn.relu)):
-            return inception.inception_resnet_v2(
-                inputs,
-                num_classes=opts.num_classes,
-                is_training=is_training,
-                dropout_keep_prob=opts.dropout_keep_prob,
-                reuse=None,
-                create_aux_logits=opts.create_aux_logits)
-    elif model_name == 'vgg16':
-        with slim.arg_scope(vgg.vgg_arg_scope(weight_decay=opts.weight_decay)):
-            return vgg.vgg_16(
-                inputs,
-                num_classes=opts.num_classes,
-                dropout_keep_prob=opts.dropout_keep_prob,
-                spatial_squeeze=opts.spatial_squeeze,
-                is_training=is_training,
-                fc_conv_padding='VALID',
-                global_pool=opts.global_pool)
-    elif model_name == 'vgg19':
-        with slim.arg_scope(vgg.vgg_arg_scope(weight_decay=opts.weight_decay)):
-            return vgg.vgg_19(
-                inputs,
-                num_classes=opts.num_classes,
-                dropout_keep_prob=opts.dropout_keep_prob,
-                spatial_squeeze=opts.spatial_squeeze,
-                is_training=is_training,
-                fc_conv_padding='VALID',
-                global_pool=opts.global_pool)
-    else:
-        raise ValueError("Unsurpported model name.")
-
-
 def model_fn(features, labels, mode, params):
     """Build model graph."""
 
@@ -139,7 +23,162 @@ def model_fn(features, labels, mode, params):
     opts = params['opts']
     inputs = features[model_keys.DATA_COL]
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
-    logits, end_points = get_model(opts.model_name, inputs, is_training, opts)
+    logits, end_points = get_model_def(
+        opts.model_name, inputs, is_training, opts)
+
+    if opts.use_moving_average:
+        variables_to_train = \
+            build_model_fn_utils.get_finetune_trainable_variables(opts)
+        ema = tf.train.ExponentialMovingAverage(opts.moving_average_decay)
+        maintain_averages_op = ema.apply(variables_to_train)
+    else:
+        ema, maintain_averages_op = None, None
 
     return build_model_fn_utils.create_estimator_spec(
-        mode, logits, labels, params)
+        mode, logits, labels, params, ema, maintain_averages_op)
+
+
+def get_model_def(model_name, inputs, is_training, opts):
+    model_def_map = {
+        'vgg16': vgg16,
+        'vgg19': vgg19,
+        'inception_v1': inception_v1,
+        'inception_v2': inception_v2,
+        'inception_v3': inception_v3,
+        'inception_v4': inception_v4,
+        'inception_resnet_v2': inception_resnet_v2,
+        'resnet_v1_50': resnet_v1_50,
+    }
+
+    if model_name not in model_def_map:
+        raise ValueError('Model name [%s] was not recognized' % model_name)
+
+    model_def = model_def_map[model_name]
+    return model_def(inputs, is_training, opts)
+
+
+def vgg16(inputs, is_training, opts):
+    with slim.arg_scope(vgg.vgg_arg_scope(weight_decay=opts.weight_decay)):
+        return vgg.vgg_16(
+            inputs,
+            num_classes=opts.num_classes,
+            dropout_keep_prob=opts.dropout_keep_prob,
+            spatial_squeeze=opts.spatial_squeeze,
+            is_training=is_training,
+            fc_conv_padding='VALID',
+            global_pool=opts.global_pool)
+
+
+def vgg19(inputs, is_training, opts):
+    with slim.arg_scope(vgg.vgg_arg_scope(weight_decay=opts.weight_decay)):
+        return vgg.vgg_19(
+            inputs,
+            num_classes=opts.num_classes,
+            dropout_keep_prob=opts.dropout_keep_prob,
+            spatial_squeeze=opts.spatial_squeeze,
+            is_training=is_training,
+            fc_conv_padding='VALID',
+            global_pool=opts.global_pool)
+
+
+def inception_v1(inputs, is_training, opts):
+    with slim.arg_scope(inception.inception_v1_arg_scope(
+            weight_decay=opts.weight_decay,
+            use_batch_norm=opts.use_batch_norm,
+            batch_norm_decay=opts.batch_norm_decay,
+            batch_norm_epsilon=opts.batch_norm_epsilon,
+            activation_fn=tf.nn.relu)):
+        return inception.inception_v1(
+            inputs,
+            num_classes=opts.num_classes,
+            is_training=is_training,
+            dropout_keep_prob=opts.dropout_keep_prob,
+            prediction_fn=slim.softmax,
+            spatial_squeeze=opts.spatial_squeeze,
+            reuse=None,
+            global_pool=opts.global_pool)
+
+
+def inception_v2(inputs, is_training, opts):
+    with slim.arg_scope(inception.inception_v2_arg_scope(
+            weight_decay=opts.weight_decay,
+            use_batch_norm=opts.use_batch_norm,
+            batch_norm_decay=opts.batch_norm_decay,
+            batch_norm_epsilon=opts.batch_norm_epsilon,
+            activation_fn=tf.nn.relu)):
+        return inception.inception_v2(
+            inputs,
+            num_classes=opts.num_classes,
+            is_training=is_training,
+            dropout_keep_prob=opts.dropout_keep_prob,
+            min_depth=opts.min_depth,
+            depth_multiplier=opts.depth_multiplier,
+            prediction_fn=slim.softmax,
+            spatial_squeeze=opts.spatial_squeeze,
+            reuse=None,
+            global_pool=opts.global_pool)
+
+
+def inception_v3(inputs, is_training, opts):
+    with slim.arg_scope(inception.inception_v3_arg_scope(
+            weight_decay=opts.weight_decay,
+            use_batch_norm=opts.use_batch_norm,
+            batch_norm_decay=opts.batch_norm_decay,
+            batch_norm_epsilon=opts.batch_norm_epsilon,
+            activation_fn=tf.nn.relu)):
+        return inception.inception_v3(
+            inputs,
+            num_classes=opts.num_classes,
+            is_training=is_training,
+            dropout_keep_prob=opts.dropout_keep_prob,
+            min_depth=opts.min_depth,
+            depth_multiplier=opts.depth_multiplier,
+            prediction_fn=slim.softmax,
+            spatial_squeeze=opts.spatial_squeeze,
+            reuse=None,
+            create_aux_logits=opts.create_aux_logits,
+            global_pool=opts.global_pool)
+
+
+def inception_v4(inputs, is_training, opts):
+    with slim.arg_scope(inception.inception_v4_arg_scope(
+            weight_decay=opts.weight_decay,
+            use_batch_norm=opts.use_batch_norm,
+            batch_norm_decay=opts.batch_norm_decay,
+            batch_norm_epsilon=opts.batch_norm_epsilon,
+            activation_fn=tf.nn.relu)):
+        return inception.inception_v4(
+            inputs,
+            num_classes=opts.num_classes,
+            is_training=is_training,
+            dropout_keep_prob=opts.dropout_keep_prob,
+            reuse=None,
+            create_aux_logits=opts.create_aux_logits)
+
+
+def inception_resnet_v2(inputs, is_training, opts):
+    with slim.arg_scope(inception.inception_resnet_v2_arg_scope(
+            weight_decay=opts.weight_decay,
+            batch_norm_decay=opts.batch_norm_decay,
+            batch_norm_epsilon=opts.batch_norm_epsilon,
+            activation_fn=tf.nn.relu)):
+        return inception.inception_resnet_v2(
+            inputs,
+            num_classes=opts.num_classes,
+            is_training=is_training,
+            dropout_keep_prob=opts.dropout_keep_prob,
+            reuse=None,
+            create_aux_logits=opts.create_aux_logits)
+
+
+def resnet_v1_50(inputs, is_training, opts):
+    with slim.arg_scope(resnet_v1.resnet_arg_scope(
+            weight_decay=opts.weight_decay,
+            batch_norm_decay=opts.batch_norm_decay,
+            batch_norm_epsilon=opts.batch_norm_epsilon,
+            activation_fn=tf.nn.relu)):
+        return resnet_v1.resnet_v1_50(inputs,
+                                      num_classes=opts.num_classes,
+                                      is_training=is_training,
+                                      global_pool=opts.global_pool,
+                                      reuse=None)

tf/slim/image_models/finetune/build_model_fn_utils.py
@@ -32,10 +32,18 @@ def get_loss(logits, labels, name, opts):
     return loss
 
 
-def create_predict_estimator_spec(mode, logits, labels, params):
+def create_predict_estimator_spec(
+        mode, logits, labels, params, ema, maintain_averages_op):
     """Create predict EstimatorSpec."""
 
-    score = tf.nn.softmax(logits)
+    if ema:
+        assign_dep = restore_moving_average_vars(ema)
+    else:
+        assign_dep = tf.no_op()
+
+    with tf.control_dependencies([assign_dep]):
+        score = tf.nn.softmax(logits)
+
     predictions = {
         'score': score,
     }
@@ -52,23 +60,34 @@ def create_predict_estimator_spec(mode, logits, labels, params):
                                       export_outputs=export_outputs)
 
 
-def create_eval_estimator_spec(mode, logits, labels, params):
+def create_eval_estimator_spec(
+        mode, logits, labels, params, ema, maintain_averages_op):
     """Create eval EstimatorSpec."""
 
     opts = params['opts']
 
-    accuracy = tf.metrics.accuracy(labels=tf.argmax(labels, 1),
-                                   predictions=tf.argmax(logits, 1))
+    if ema:
+        assign_dep = restore_moving_average_vars(ema)
+    else:
+        assign_dep = tf.no_op()
+
+    with tf.control_dependencies([assign_dep]):
+        accuracy = tf.metrics.accuracy(
+            labels=tf.argmax(labels, 1),
+            predictions=tf.argmax(logits, 1))
+
     metrics = {
         'accuracy': accuracy
     }
+
     add_metrics_summary(metrics)
 
     loss = get_loss(logits, labels, 'eval', opts)
     return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
 
 
-def create_train_estimator_spec(mode, logits, labels, params):
+def create_train_estimator_spec(
+        mode, logits, labels, params, ema, maintain_averages_op):
     """Create train EstimatorSpec."""
 
     opts = params['opts']
@@ -80,23 +99,26 @@ def create_train_estimator_spec(mode, logits, labels, params):
         tf.cast(tf.equal(tf.argmax(labels, 1), tf.argmax(logits, 1)),
                 tf.float32)))
 
-    opts = params['opts']
-    num_samples_per_epoch = params['num_samples_per_epoch']
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    if maintain_averages_op:
+        update_ops.append(maintain_averages_op)
 
-    global_step = tf.train.get_global_step()
+    opts = params['opts']
     loss = get_loss(logits, labels, 'train', opts)
+    num_samples_per_epoch = params['num_samples_per_epoch']
+    global_step = tf.train.get_global_step()
     learning_rate = configure_learning_rate(
         num_samples_per_epoch, global_step, opts)
     tf.summary.scalar('learning_rate', learning_rate)
     optimizer = configure_optimizer(learning_rate, opts)
 
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)  # for bn
+    variables_to_train = get_finetune_trainable_variables(opts)
     train_op = tf.contrib.training.create_train_op(
         total_loss=loss,
         optimizer=optimizer,
         global_step=global_step,
         update_ops=update_ops,
-        variables_to_train=get_finetune_trainable_variables(opts),
+        variables_to_train=variables_to_train,
         transform_grads_fn=None,
         summarize_gradients=True,
         aggregation_method=None,
@@ -107,15 +129,19 @@ def create_train_estimator_spec(mode, logits, labels, params):
         mode, loss=loss, train_op=train_op, training_hooks=training_hooks)
 
 
-def create_estimator_spec(mode, logits, labels, params):
+def create_estimator_spec(
+        mode, logits, labels, params, ema, maintain_averages_op):
     if mode == tf.estimator.ModeKeys.PREDICT:
-        return create_predict_estimator_spec(mode, logits, labels, params)
+        return create_predict_estimator_spec(
+            mode, logits, labels, params, ema, maintain_averages_op)
 
     if mode == tf.estimator.ModeKeys.EVAL:
-        return create_eval_estimator_spec(mode, logits, labels, params)
+        return create_eval_estimator_spec(
+            mode, logits, labels, params, ema, maintain_averages_op)
 
     if mode == tf.estimator.ModeKeys.TRAIN:
-        return create_train_estimator_spec(mode, logits, labels, params)
+        return create_train_estimator_spec(
+            mode, logits, labels, params, ema, maintain_averages_op)
 
 
 def add_metrics_summary(metrics):
@@ -237,3 +263,11 @@ def configure_learning_rate(num_samples_per_epoch, global_step, opts):
     else:
         raise ValueError('learning_rate_decay_type [%s] was not recognized' %
                          opts.learning_rate_decay_type)
+
+
+def restore_moving_average_vars(ema):
+    ema_variables = tf.get_collection(tf.GraphKeys.MOVING_AVERAGE_VARIABLES)
+    tf.logging.info("ExponentialMovingAverage variables:")
+    for var in ema_variables:
+        tf.logging.info(var)
+    return tf.group(*[tf.assign(x, ema.average(x)) for x in ema_variables])

tf/slim/image_models/finetune/input_data.py
@@ -14,7 +14,7 @@ import model_keys
 DATA_COL = model_keys.DATA_COL
 
 
-def read_txt_file(txt_file, has_label=True):
+def read_txt_file(txt_file, has_label):
     """Read the content of the text file and store it into lists."""
 
     img_paths = []
@@ -56,10 +56,11 @@ def shuffle_lists(img_paths, labels):
     return shuffle_img_paths, shuffle_labels
 
 
-def create_image_dataset(data_path, has_label=True):
+def create_image_dataset(data_path, has_label, shuffle):
     if has_label:
         img_paths, img_labels = read_txt_file(data_path, has_label=True)
-        img_paths, img_labels = shuffle_lists(img_paths, img_labels)
+        if shuffle:
+            img_paths, img_labels = shuffle_lists(img_paths, img_labels)
         img_paths = tf.convert_to_tensor(img_paths, dtype=tf.string)
         labels = tf.convert_to_tensor(img_labels, dtype=tf.int32)
         ds = tf.data.Dataset.from_tensor_slices((img_paths, labels))
@@ -94,7 +95,7 @@ def parse_function(img_path, label, is_training, opts, image_size):
 
 def build_train_input_fn(opts, data_path):
     def train_input_fn():
-        ds = create_image_dataset(data_path, has_label=True)
+        ds = create_image_dataset(data_path, has_label=True, shuffle=True)
         ds = ds.map(
             lambda filename, label: parse_function(
                 filename, label, True, opts, opts.train_image_size),
@@ -111,7 +112,7 @@ def build_train_input_fn(opts, data_path):
 
 def build_eval_input_fn(opts, data_path):
     def eval_input_fn():
-        ds = create_image_dataset(data_path, has_label=True)
+        ds = create_image_dataset(data_path, has_label=True, shuffle=False)
         ds = ds.map(
             lambda filename, label: parse_function(
                 filename, label, False, opts, opts.inference_image_size),
@@ -124,7 +125,7 @@ def build_eval_input_fn(opts, data_path):
 
 def build_predict_input_fn(opts, data_path):
     def predict_input_fn():
-        ds = create_image_dataset(data_path, has_label=False)
+        ds = create_image_dataset(data_path, has_label=False, shuffle=False)
         ds = ds.map(
             lambda filename: parse_function(
                 filename, None, False, opts, opts.inference_image_size),

tf/slim/image_models/finetune/train.py
@@ -124,9 +124,14 @@ def predict(opts):
     estimator = build_estimator(opts)
     build_predict_input_fn = input_data.build_predict_input_fn(
         opts, opts.predict_data_path)
+
+    checkpoint_path = opts.predict_checkpoint_path
+    if tf.gfile.IsDirectory(opts.predict_checkpoint_path):
+        checkpoint_path = tf.train.latest_checkpoint(checkpoint_path)
+
     results = estimator.predict(
         input_fn=build_predict_input_fn,
-        checkpoint_path=opts.predict_checkpoint_path,
+        checkpoint_path=checkpoint_path,
         yield_single_examples=True)
 
     with open(opts.predict_output, 'w') as fout, \