798402278f8d09b9094b3be74c6af677cd55c214
zhezhaoxu <zhezhaoxu@tencent.com>
2018-11-08 12:01:14 +0800

    add readme

--- tf/slim/image_models/image2vec/image2vec.sh
+++ tf/slim/image_models/image2vec/image2vec.sh
@@ -5,11 +5,23 @@ set -e
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${MYDIR}
 
+# image path file, one per line
 input='test.txt'
+
+# image features, one per line
 output='features.txt'
+
+# batch size, as large as the machine can handle
 batch_size=32
-model_name=resnet_v2_50
-preprocessing_name=inception
+
+# one of 'resnet_v2_50', 'inception'
+model_name='resnet_v2_50'
+
+# one of 'inception', 'vgg'
+# suggested config:
+#   resnet_v2_50: inception, 299
+#   inception_v3: inception, 299
+preprocessing_name='inception'
 image_size=299
 
 python image2vec.py \
--- tf/slim/image_models/image2vec/nn.sh
+++ tf/slim/image_models/image2vec/nn.sh
@@ -1,1 +1,12 @@
-python nn.py --dict test.txt --image_features features.txt --k 100 --output nn.txt
+#! /usr/bin/env bash
+
+set -e
+
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${MYDIR}
+
+python nn.py \
+    --dict test.txt \
+    --image_features features.txt \
+    --output nn.txt \
+    --k 100
