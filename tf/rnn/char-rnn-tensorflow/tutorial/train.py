0fc9a13b7d9d3cd217c1ab7faa87547e1dbd502e
zhezhaoxu <zhezhaoxu@tencent.com>
2018-11-13 20:29:48 +0800

    update rnn

--- tf/rnn/char-rnn-tensorflow/tutorial/train.py
+++ tf/rnn/char-rnn-tensorflow/tutorial/train.py
@@ -13,6 +13,8 @@ import time
 
 
 data_path = './data/tinyshakespeare/input.txt'
+checkpoint_dir = './model_dir'
+checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
 
 text = open(data_path).read()
 print ('Length of text: {} characters'.format(len(text)))
@@ -126,15 +128,72 @@ it = dataset.make_initializable_iterator()
 inp, target = it.get_next()
 
 predictions = model(inp)
-loss = loss_function(target, predictions)
+loss_tensor = loss_function(target, predictions)
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-train_op = optimizer.minimize(loss)
+train_op = optimizer.minimize(loss_tensor)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
     sess.run(it.initializer)
 
-    for step in xrange(100):
-        _, v = sess.run([train_op, loss])
-        print("loss = {}".format(v))
+    for step in xrange(200):
+        _, loss = sess.run([train_op, loss_tensor])
+        print("loss = {}".format(loss))
+
+    model.save_weights(checkpoint_prefix)
+
+
+# restore
+model = Model(vocab_size, embedding_dim, units)
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+
+# Evaluation step (generating text using the learned model)
+
+# Number of characters to generate
+num_generate = 1000
+
+# You can change the start string to experiment
+start_string = 'Q'
+
+# Converting our start string to numbers (vectorizing)
+input_eval = [char2idx[s] for s in start_string]
+input_eval = tf.expand_dims(input_eval, 0)
+
+# Empty string to store our results
+text_generated = []
+
+# Low temperatures results in more predictable text.
+# Higher temperatures results in more surprising text.
+# Experiment to find the best setting.
+temperature = 1.0
+
+# Evaluation loop.
+
+# Here batch size == 1
+model.reset_states()
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        # remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
+
+        # using a multinomial distribution to predict the word returned by the
+        # model
+        predictions = predictions / temperature
+        predicted_id = tf.multinomial(predictions, num_samples=1)[-1, 0]
+
+        # We pass the predicted word as the next input to the model
+        # along with the previous hidden state
+        input_eval = tf.expand_dims([predicted_id], 0)
+
+        predicted_id = sess.run(predicted_id)
+
+        text_generated.append(idx2char[predicted_id])
+
+        print (start_string + ''.join(text_generated))
