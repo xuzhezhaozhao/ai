ad6cf45b51ac5cd2a375fcb144c63116c69c8450
zhezhaoxu <zhezhaoxu@tencent.com>
2018-11-13 15:28:27 +0800

    update

--- tf/slim/image_models/image2vec/cat_vs_dog_classifier/preprocess.sh
+++ tf/slim/image_models/image2vec/cat_vs_dog_classifier/preprocess.sh
@@ -1,11 +1,13 @@
 
+OPTS=''
+
 awk '{print $1}' train.txt > train_images.txt
 awk '{print $2}' train.txt > train_labels.txt
-../image2vec --input train_images.txt --output train_features.txt
+../image2vec ${OPTS} --input train_images.txt --output train_features.txt
 
 awk '{print $1}' validation.txt > validation_images.txt
 awk '{print $2}' validation.txt > validation_labels.txt
-../image2vec --input validation_images.txt --output validation_features.txt
+../image2vec ${OPTS} --input validation_images.txt --output validation_features.txt
 
 awk '{print $1}' test.txt > test_images.txt
-../image2vec --input test_images.txt --output test_features.txt
+../image2vec ${OPTS} --input test_images.txt --output test_features.txt
