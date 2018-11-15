ce08ee38b26157f80bfff55ccd5f8aa9789dbeb0
zhezhaoxu <zhezhaoxu@tencent.com>
2018-11-15 16:41:02 +0800

    update wget_cover.py

--- datasets/video-cover-dataset/wget_cover.py
+++ datasets/video-cover-dataset/wget_cover.py
@@ -2,11 +2,12 @@ from PIL import Image
 import os
 
 output_dir = 'train'
+print("wget ...")
 for index, line in enumerate(open('./cover.txt')):
     if index < 1:
         continue
 
-    if index % 500 == 0:
+    if index % 1000 == 0:
         print("wget {} ...".format(index))
 
     line = line.strip()
@@ -15,8 +16,19 @@ for index, line in enumerate(open('./cover.txt')):
     url = tokens[1]
     filename = os.path.join(output_dir, rowkey + '_' + str(index))
     cmd = 'wget ' + "'" + url + "'" + ' -O ' + filename + ' > wget.log 2>&1'
-    os.system('mkdir -p ' + output_dir)
-    os.system(cmd)
+    try:
+        os.system('mkdir -p ' + output_dir)
+        os.system(cmd)
+    except Exception as e:
+        print("catch Exception: {}".format(e))
+
+print("wget done.")
+
+print("rename ...")
+filenames = os.listdir(output_dir)
+for index, name in enumerate(filenames):
+    if index % 1000 == 0:
+        print("rename {} ...".format(index))
 
     img = Image.open(filename)
     img_type = img.format.lower()
@@ -28,3 +40,4 @@ for index, line in enumerate(open('./cover.txt')):
         print("shape error: " + filename + ", " + str(img.size))
 
     os.rename(filename, filename + '.' + img_type)
+print("rename done")
