94ffb5c1ea1be41de4d7101820f3af24c9d78c9a
zhezhaoxu <zhezhaoxu@tencent.com>
2019-01-08 10:43:07 +0800

    add text-cnn

---
+++ nlp/sentiment/text-cnn/word2vec.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env bash
+
+set -e
+
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${MYDIR}
+
+mkdir -p model
+datadir=../../../datasets/kd_video_comments-dataset/data/fasttext
+
+lr=0.025
+ws=5
+epoch=20
+minCount=5
+neg=10
+bucket=2000000
+minn=0
+maxn=0
+thread=7
+t=1e-4
+../../../submodules/fastText/fasttext \
+    skipgram \
+    -input ${datadir}/train.txt \
+    -output model/word2vec \
+	-lr ${lr} \
+    -dim 50 \
+	-ws ${ws} \
+	-epoch ${epoch} \
+	-minCount ${minCount} \
+    -minCountLabel 2 \
+	-neg ${neg} \
+	-loss ns \
+	-bucket ${bucket} \
+  	-minn ${minn} \
+	-maxn ${maxn} \
+	-thread ${thread} \
+	-t ${t} \
+	-lrUpdateRate 100
